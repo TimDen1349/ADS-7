80db8308407b2d86911739d73f2e92f939f73f82
TimDen1349 <tim.denisov.amur1@yandex.ru>
2026-05-16 16:27:01 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,48 +4,37 @@
 Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCar(bool light) {
-    Car* newCar = new Car{ light, nullptr, nullptr };
-    if (first == nullptr) {
-        first = newCar;
-        first->next = first;
-        first->prev = first;
-    }
-    else {
-        Car* last = first->prev;
-        last->next = newCar;
-        newCar->prev = last;
-        newCar->next = first;
-        first->prev = newCar;
-    }
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (first == nullptr) {
+    first = newCar;
+    first->next = first;
+    first->prev = first;
+  } else {
+    Car* last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
 }
 
 int Train::getLength() {
-    if (first == nullptr) return 0;
-    countOp = 0;
-    bool startLight = first->light;
-    int length = 0;
-
-    while (true) {
-        int steps = 0;
-        Car* cur = first;
-        do {
-            cur = cur->next;
-            steps++;
-            countOp++;
-        } while (cur->light != startLight);
-        cur->light = !cur->light;
-        for (int i = 0; i < steps; ++i) {
-            cur = cur->prev;
-            countOp++;
-        }
-        if (first->light != startLight) {
-            length = steps;
-            break;
-        }
+  if (first == nullptr) return 0;
+  countOp = 0;
+  bool startLight = first->light;
+  Car* cur = first;
+  int steps = 0;
+  do {
+    cur = cur->next;
+    countOp++;
+    if (cur->light == startLight) {
+      cur->light = !cur->light;
     }
-    return length;
+    steps++;
+  } while (cur != first);
+  return steps;
 }
 
 int Train::getOpCount() {
-    return countOp;
+  return countOp;
 }
