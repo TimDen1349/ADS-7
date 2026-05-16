9dee84fce5608a7e1f91423589c75c84e6e261be
TimDen1349 <tim.denisov.amur1@yandex.ru>
2026-05-16 16:12:54 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,51 +4,52 @@
 Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCar(bool light) {
-    Car* newCar = new Car{ light, nullptr, nullptr };
-    if (first == nullptr) {
-        first = newCar;
-        first->next = first;
-        first->prev = first;
-    }
-    else {
-        Car* last = first->prev;
-        last->next = newCar;
-        newCar->prev = last;
-        newCar->next = first;
-        first->prev = newCar;
-    }
+  Car* newCar = new Car{light, nullptr, nullptr};
+  if (first == nullptr) {
+    first = newCar;
+    first->next = first;
+    first->prev = first;
+  } else {
+    Car* last = first->prev;
+    last->next = newCar;
+    newCar->prev = last;
+    newCar->next = first;
+    first->prev = newCar;
+  }
 }
 
 int Train::getLength() {
-    if (first == nullptr) return 0;
-    countOp = 0;
-    bool originalLight = first->light;
-    int length = 0;
+  if (first == nullptr) {
+    return 0;
+  }
+  countOp = 0;
+  bool originalLight = first->light;
+  int length = 0;
 
-    while (true) {
-        int steps = 0;
-        Car* cur = first;
-        do {
-            cur = cur->next;
-            steps++;
-            countOp++;
-        } while (cur->light != first->light && cur != first);
+  while (true) {
+    int steps = 0;
+    Car* cur = first;
+    do {
+      cur = cur->next;
+      steps++;
+      countOp++;
+    } while (cur->light != first->light && cur != first);
 
-        cur->light = !cur->light;
+    cur->light = !cur->light;
 
-        for (int i = 0; i < steps; ++i) {
-            cur = cur->prev;
-            countOp++;
-        }
-
-        if (first->light != originalLight) {
-            length = steps;
-            break;
-        }
+    for (int i = 0; i < steps; ++i) {
+      cur = cur->prev;
+      countOp++;
     }
-    return length;
+
+    if (first->light != originalLight) {
+      length = steps;
+      break;
+    }
+  }
+  return length;
 }
 
-int Train::getOpCount() const {
-    return countOp;
+int Train::getOpCount() {
+  return countOp;
 }
