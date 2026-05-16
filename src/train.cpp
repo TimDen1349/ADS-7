1692c647237c2f08d03cae4f840b43779e36b8fc
TimDen1349 <tim.denisov.amur1@yandex.ru>
2026-05-16 16:33:57 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -9,39 +9,32 @@ void Train::addCar(bool light) {
     first = newCar;
     first->next = first;
     first->prev = first;
-  } else {
-    Car* last = first->prev;
-    last->next = newCar;
-    newCar->prev = last;
-    newCar->next = first;
-    first->prev = newCar;
+    return;
   }
+  Car* last = first->prev;
+  last->next = newCar;
+  newCar->prev = last;
+  newCar->next = first;
+  first->prev = newCar;
 }
 
 int Train::getLength() {
   if (first == nullptr) return 0;
   countOp = 0;
-  bool startLight = first->light;
-  int step = 0;
-
-  while (true) {
-    step++;
-    Car* cur = first;
-    for (int i = 0; i < step; ++i) {
-      cur = cur->next;
-      countOp++;
-    }
-    if (cur->light == startLight) {
-      cur->light = !cur->light;
-      for (int i = 0; i < step; ++i) {
-        cur = cur->prev;
-        countOp++;
-      }
-      if (first->light != startLight) {
-        return step;
-      }
-    }
+  bool original = first->light;
+  first->light = !original;
+  int steps = 0;
+  Car* cur = first;
+  do {
+    cur = cur->next;
+    ++countOp;
+    ++steps;
+  } while (cur->light != first->light);
+  for (int i = 0; i < steps; ++i) {
+    cur = cur->prev;
+    ++countOp;
   }
+  return steps;
 }
 
 int Train::getOpCount() {
